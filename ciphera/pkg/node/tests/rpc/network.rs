a0386e82070effa57c4d219608ca8cb44da2ce90
Ilya Evdokimov <evdokimov.ilya@gmail.com>
2026-03-10 17:10:17 +0100

    contract check removed, chain id added

--- ciphera/pkg/node/tests/rpc/network.rs
+++ ciphera/pkg/node/tests/rpc/network.rs
@@ -1,4 +1,3 @@
-use constants::MERKLE_TREE_DEPTH;
 use testutil::eth::EthNode;
 
 use crate::rpc::ServerConfig;
@@ -10,6 +9,5 @@ async fn network_info() {
     let eth_node = EthNode::default().run_and_deploy().await;
     let server = Server::setup_and_wait(ServerConfig::single_node(false), eth_node).await;
     let resp = server.network().await.unwrap();
-    assert_eq!(resp.rollup_contract, "0xx");
-    assert_eq!(resp.chain_id, 5115);
+    assert_eq!(resp.chain_id, 5655);
 }
